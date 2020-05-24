904c916960bc096a5c33229c5e2428dd6bfe95e3
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-24 16:16:10 -0700

    Web_Scraper

--- lib/web_scraper.rb
+++ lib/web_scraper.rb
@@ -1,6 +1,23 @@
 require 'open-uri'
 require 'nokogiri'
 require 'net/http'
+require 'json'
+require 'rest-client'
+require 'pry'
 
 class WebScraper
+    @@url = 'https://jobs.github.com/positions.json'
+    def api
+        uri = URI.parse(@@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+        puts JSON.parse(response.body)
+    end
+
+    def get_request
+        rest = RestClient.get(@@url)
+        data = JSON.parse(rest.body)
+    end
 end
+
+WebScraper.new.get_request
